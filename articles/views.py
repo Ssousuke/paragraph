40c1201b5e088a63630fa59987cf3a18dabba049
Ssousuke <wesleysousukee@gmail.com>
2022-01-21 10:06:09 -0300

    remove import

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,4 @@
 from rest_framework.views import APIView
-from rest_framework import authentication, permissions
 from articles.models import Articles
 from rest_framework.response import Response
 from articles.serializer import CategorySerializer, ArticleSerializer
