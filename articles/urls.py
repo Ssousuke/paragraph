9ceed7988884eb1e4a2d670a8961dbd1ce4b24c0
Ssousuke <wesleysousukee@gmail.com>
2022-01-22 17:44:27 -0300

    implementa método delete

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.ListArticle.as_view(), name='list-articles'),
     path('criar/', views.ListArticle.as_view(), name='create-articles'),
+    path('deletar/<int:pk>', views.ListArticle.as_view(), name='delete-articles'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from django.http import Http404
 from articles.models import Articles
 from rest_framework.response import Response
 from articles.serializer import CategorySerializer, ArticleSerializer
@@ -17,16 +18,27 @@ class ListArticle(APIView):
         list_articles = Articles.objects.filter(publish=True)
         return list_articles
 
+    def get_object(self, pk):
+        try:
+            return Articles.objects.get(pk=pk)
+        except Articles.DoesNotExist:
+            raise Http404
+
     def get(self, request):
         """
         Retorna todos os artigos publicados
         """
         list_articles = self.get_queryset()
         serializer = ArticleSerializer(list_articles, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, request):
         serializer = ArticleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
